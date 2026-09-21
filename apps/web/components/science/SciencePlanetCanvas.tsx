caa4b92d0109e854e614b8304f8b4a67fc43619d
Cindy Pua <cindyqi0511@gmail.com>
2026-09-21 23:10:08 +0800

    feat: play Colors Canyon and Life Cycle Garden camera-first and retire the old starter session

--- apps/web/components/science/SciencePlanetCanvas.tsx
+++ apps/web/components/science/SciencePlanetCanvas.tsx
@@ -9,11 +9,9 @@ import { scienceLand } from "./scienceLands";
 import { MagnetLabMission } from "./MagnetLabMission";
 import { ScienceExplorerBridge } from './ScienceExplorerBridge';
 import { ScienceSessionFrame } from './ScienceSessionFrame';
-import { ScienceStarterSession, type StarterProgress } from './ScienceStarterSession';
 import { ScienceHandSession } from './ScienceHandSession';
-import { BENCH_SETS } from '../handActivity/sets';
 import { isEntryKey, LAND_GUIDES } from './scienceInvitation';
-import type { StarterLand } from './scienceActivities';
+import type { StarterLand, StarterProgress } from './scienceActivities';
 import sessionStyles from './ScienceSession.module.css';
 import styles from "./sciencePlanet.module.css";
 
@@ -79,8 +77,6 @@ export function SciencePlanetCanvas(props: SciencePlanetCanvasProps) {
     />
     </div>
     {active ? <div className={sessionStyles.magnetOverlay} data-session-controls><MagnetLabMission manageFocus={false} onExit={close} onComplete={() => setCompleted(true)} /></div> : null}
-    {starter ? (BENCH_SETS[starter]
-      ? <ScienceHandSession key={starter} land={starter} progress={progress[starter]} onProgress={value => setProgress(previous => ({ ...previous, [starter]: value }))} onClose={close} />
-      : <ScienceStarterSession key={starter} land={starter} graphics={available} progress={progress[starter]} onProgress={value => setProgress(previous => ({ ...previous, [starter]: value }))} onClose={close} />) : null}
+    {starter ? <ScienceHandSession key={starter} land={starter} progress={progress[starter]} onProgress={value => setProgress(previous => ({ ...previous, [starter]: value }))} onClose={close} /> : null}
   </ScienceSessionFrame>;
 }
